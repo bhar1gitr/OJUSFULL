cc2def1b5b5f73ee5ccd3b0f25f79f3e9099d3aa
Bharat Sharma <21104023bharatsharma@gmail.com>
2024-12-18 11:33:28 +0530

    Update Gallery.jsx

--- src/Sports/Gallery.jsx
+++ src/Sports/Gallery.jsx
@@ -1,58 +1,70 @@
-import React from "react";
-import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.min.css";
-import "./Gallery.css"; // Import the CSS
-import image1 from "../assets/image1copy.jpg";
-import image2 from "../assets/image1copy.jpg";
-import image3 from "../assets/image3copy.jpg";
-import image4 from "../assets/image4copy.jpg";
+// import React from "react";
+// import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/swiper-bundle.min.css";
+// import "./Gallery.css"; // Import the CSS
+// import image1 from "../assets/image1copy.jpg";
+// import image2 from "../assets/image1copy.jpg";
+// import image3 from "../assets/image3copy.jpg";
+// import image4 from "../assets/image4copy.jpg";
 
 
-SwiperCore.use([EffectCoverflow, Pagination]);
+// SwiperCore.use([EffectCoverflow, Pagination]);
+
+// const Gallery = () => {
+//   const images = [
+//     { src: image1, number: "01" },
+//     { src: image2, number: "02" },
+//     { src: image3, number: "03" },
+//     { src: image4, number: "04" },
+//     { src: image1, number: "05" },
+//   ];
+
+//   return (
+//     <div className="swiper-container">
+//     {/* aewf */}
+//       <Swiper
+//         effect="coverflow"
+//         grabCursor={true}
+//         centeredSlides={true}
+//         slidesPerView="auto"
+//         loop={true}
+//         pagination={{ clickable: true }}
+//         coverflowEffect={{
+//           rotate: 50,
+//           stretch: 0,
+//           depth: 100,
+//           modifier: 1,
+//           slideShadows: true,
+//         }}
+//       >
+//         {images.map((image, index) => (
+//           <SwiperSlide key={index}>
+//             <div
+//               className="slide-content"
+//               style={{
+//                 backgroundImage: `url(${image.src})`,
+//               }}
+//             >
+              
+//             </div>
+//           </SwiperSlide>
+//         ))}
+//       </Swiper>
+//     </div>
+//   );
+// };
+
+// export default Gallery;
+
+
+import React from 'react'
 
 const Gallery = () => {
-  const images = [
-    { src: image1, number: "01" },
-    { src: image2, number: "02" },
-    { src: image3, number: "03" },
-    { src: image4, number: "04" },
-    { src: image1, number: "05" },
-  ];
-
   return (
-    <div className="swiper-container">
-    {/* aewf */}
-      <Swiper
-        effect="coverflow"
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView="auto"
-        loop={true}
-        pagination={{ clickable: true }}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-      >
-        {images.map((image, index) => (
-          <SwiperSlide key={index}>
-            <div
-              className="slide-content"
-              style={{
-                backgroundImage: `url(${image.src})`,
-              }}
-            >
-              
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-  );
-};
+    <div>Gallery</div>
+  )
+}
 
-export default Gallery;
+export default Gallery
+
